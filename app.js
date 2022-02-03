6efcaf44385b5da2267638a55c896b7e9f79feb6
wongsie <75053096+wongsie@users.noreply.github.com>
2022-02-03 14:20:59 +0100

    initial upload of js code

---
+++ app.js
@@ -0,0 +1,15 @@
+const https = require(https);
+
+const port = process.env.PORT;
+
+https.createServer(function (req, res) {
+  let url = req.url;
+
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  res.write('<h1>Hello world!</h1>');
+  res.end();
+}).listen(port, function() {
+  console.log("server start at port.")
+});
